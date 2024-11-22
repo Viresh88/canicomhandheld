85c80bf81de1cd194b91c852aed8a55dba8a4f54
Viresh <viresh7781@gmail.com>
2024-11-22 19:25:17 +0530

    calendar doglis screen design

---
+++ app/src/main/java/com/example/canicomhandheld/CalendarAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.canicomhandheld
+
+class CalendarAdapter {
+}
---
+++ app/src/main/java/com/example/canicomhandheld/DogList.kt
@@ -0,0 +1,11 @@
+package com.example.canicomhandheld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DogList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_list)
+    }
+}
